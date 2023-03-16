1856339c83116783976708e278e3e5657b203c42
dleowns1102 <dleowns1102@gmail.com>
2023-03-16 13:05:17 +0900

    go 로 TDD 시작하기

---
+++ board_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateBoard(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal(123, 123, "Error")
+}
