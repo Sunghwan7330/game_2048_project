7d81e7086260c6388c960d3764bafa327836eff3
Sunghwan <chak1212@naver.com>
2023-03-21 13:49:56 +0900

    main 함수 틀 추가

--- main.go
+++ main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+func initGame() {
+	fmt.Println("Welcome to game 2048.")
+	//TODO Input board size. And init Board
+}
+
+func doingGame() {
+	fmt.Println("Start Game")
+	//TODO add logic
+}
+
+func endGame() {
+	fmt.Println("Game over.")
+	//TODO print game result. (score)
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	initGame()
+	doingGame()
+	endGame()
 }
